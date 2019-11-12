387c9ad2c6e62a99373dee469688d7c669d1dfcf
610533 <610533@0338S-S-R0B6ZHA.sdcs.local>
2019-11-12 14:23:08 -0800

    Finished some modules

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -5,23 +5,49 @@ package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
 import java.awt.Color;
-
+import org.jointheleague.graphical.robot.Robot;
 public class RobotColorChooser {
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot Rob = new Robot();
 		//3. Ask the user what color they would like the robot to draw
-		
+		for (int i = 0; i < 5; i++) {
+		String red = JOptionPane.showInputDialog("What color would you like the robot to draw?");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+		if (red.equalsIgnoreCase("red")) {
+			Rob.setPenColor(Color.red);	
+		}
+		else if (red.equalsIgnoreCase("blue")) {
+			Rob.setPenColor(Color.blue);
+		}
+		else if (red.equalsIgnoreCase("green")) {
+			Rob.setPenColor(Color.green);
+		}
+		else if (red.equalsIgnoreCase("yellow")) {
+			Rob.setPenColor(Color.yellow);
+		}
+		else if (red.equalsIgnoreCase("black")) {
+			Rob.setPenColor(Color.black);
+		}
+		else if (red.equalsIgnoreCase("pink")) {
+			Rob.setPenColor(Color.pink);
+		}
+		else Rob.setRandomPenColor();
         //6. If the user doesn’t enter anything, choose a random color
-
+		
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		
+		Rob.setPenWidth(10);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
+		Rob.setSpeed(200);
+		Rob.penDown();
+		for (int b = 0; b < 4; b++) {
+		Rob.move(100);
+		Rob.turn(90);	
+		}
+	
 
 	}
+}
 }
--- src/_02_crazy_cat_lady/CrazyCatLady.java
+++ src/_02_crazy_cat_lady/CrazyCatLady.java
@@ -5,20 +5,29 @@ package _02_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+	String kenny = 	JOptionPane.showInputDialog("How many cats do you have?");
 		// 2. Convert their answer into an int
-
+		int kenny2 = Integer.parseInt(kenny);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+		if (kenny2 >= 3) {
+			JOptionPane.showMessageDialog(null, "You crazy cat lady... stay away with all them cats...");
+		}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+		if (kenny2 <= 2 && kenny2 >=1) {
+			JOptionPane.showMessageDialog(null, "You're not crazy.");
+			playVideo("https://www.youtube.com/watch?v=XyNlqQId-nk");
+		}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+		if (kenny2 == 0) {
+			JOptionPane.showMessageDialog(null, "bruh");
+			playVideo("https://www.youtube.com/watch?v=sxAlWUSkACs");
 	}
-
+	}
 	static void playVideo(String videoURL) {
 		try {
 			URI uri = new URI(videoURL);
